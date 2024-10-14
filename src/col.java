371b75808f898182bd0e4fd6709eb6bda9e3eec2
ayushchaubey17 <ayushreso1234@gmail.com>
2024-10-14 18:39:18 +0530

    collections

---
+++ src/col.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class col {
+    public static void main(String[] args) {
+   List<Integer> al = new ArrayList<>();
+
+    al.add(12);
+        System.out.println(al);
+
+    }
+}
